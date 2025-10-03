2cda76e1510b46d3562daabf90ea8fd06aff2884
Adarsh Raj <mt1240757@iitd.ac.in>
2025-10-03 16:04:47 +0530

    episode01

---
+++ app.js
@@ -0,0 +1,10 @@
+const heading=React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child1"}),
+        [React.createElement("h1",{},"i am h1 tag"),
+        React.createElement("h2",{},"i am h2 tag")],
+    React.createElement("div",{id:"child2"}),
+        [React.createElement("h1",{},"i am h1 tag"),
+        React.createElement("h2",{},"i am h2 tag")]]
+)
+const Root=ReactDOM.createRoot(document.getElementById("root"));
+Root.render(heading);
