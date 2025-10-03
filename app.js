f979fcb5c7d1f6dded07227b10d10dc01c311bc5
Adarsh Raj <mt1240757@iitd.ac.in>
2025-10-04 00:36:36 +0530

    episode02

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const heading=React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child1"}),
-        [React.createElement("h1",{},"i am h1 tag"),
-        React.createElement("h2",{},"i am h2 tag")],
-    React.createElement("div",{id:"child2"}),
-        [React.createElement("h1",{},"i am h1 tag"),
-        React.createElement("h2",{},"i am h2 tag")]]
-)
-const Root=ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child1", key: "child1" },
+      [
+        React.createElement("h1", { key: "child1-h1" }, "I am h1 tag"),
+        React.createElement("h2", { key: "child1-h2" }, "I am fgfdfgffgfrgfrtgfrth2 tag")
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child2", key: "child2" },
+      [
+        React.createElement("h1", { key: "child2-h1" }, "I am h1dfdlfd tag"),
+        React.createElement("h2", { key: "child2-h2" }, "I am h2 ertretag")
+      ]
+    )
+  ]
+);
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
 Root.render(heading);
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
 # react
+
+# parcel 
+ -Dev build
+ -Local Server
+ -HMR:Hot Module Replacement
+ -File Watching Algorithm in c++
+ -Cacheing :Faster build
+ -Image Optimization
+ -Minification
+ -Bundling
+ -Compressing
+ -Consistent Hashing 
+ -code splitting 
+ -differential bundling - support older browser
+ -Daignostics
+ -Error Handlig
+ -HTTPs
+ -Tree Shaking Algorithm
