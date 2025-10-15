ae2618da27912c2acc9d964247d306d4a3c5e1da
Adarsh Raj <mt1240757@iitd.ac.in>
2025-10-15 17:57:07 +0530

    episode06

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,14 +4,19 @@ import resObj from "../utils/mockdata";
 const Body=()=>{
     const [listOfRestaurant,setListOfRestaurant]=useState(resObj);
     const [searchText,setsearchText]=useState("");
+
     // console.log("body rerender");
     // useEffect(()=>{
     //   fetchData();
     // },[])
     // const fetchData= async ()=>{
-    //   const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5492263&lng=77.1858229&collection=80402&tags=layout_Shawarma_Contextual&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+    //   const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5492263&lng=77.1858229&collection=83636&tags=layout_CCS_Chinese&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
     //   const json= await data.json();
-    //   setListOfRestaurant();
+    //   const restaurants = json.data.cards
+    //     .map(cardWrapper => cardWrapper.card.card)
+    //     .filter(card => card['@type'] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant");
+    //   setNewListOfRestaurant(restaurants);
+    //   setListOfRestaurant(restaurants);
     // }
     return(
       <div className="Body">
@@ -20,7 +25,7 @@ const Body=()=>{
               <input type="text" className="searchBox" value={searchText} onChange={(e)=>{
                 const value=e.target.value.toLowerCase();
                 setsearchText(value);
-               const tempsearchObj=resObj.filter(
+                const tempsearchObj=resObj.filter(
                 (res)=>{
                   let restaurantName=res.info.name.toLowerCase();
                   let flag=1;
@@ -32,13 +37,34 @@ const Body=()=>{
                   return flag===1;
                 }
               )
+              const temp=resObj.filter((res)=>{
+                let newres=res.info.name.toLowerCase();
+                return newres.includes(value);
+                })
+              tempsearchObj.push(...temp);
+              const seen = new Set();
+              const combined = tempsearchObj.filter(res => {
+                if (seen.has(res.info.id)) return false;
+                  seen.add(res.info.id);
+                  return true;
+                  });
               if(value.length===0){
                 setListOfRestaurant(resObj);
               }else{
-                setListOfRestaurant(tempsearchObj);
+                setListOfRestaurant(combined);
               }
               //  console.log(searchText);
-              }}></input>
+              }}
+              onKeyDown={(e)=>{
+                if(e.key==="Enter"){
+                  const temp=resObj.filter((res)=>{
+                    let newres=res.info.name.toLowerCase();
+                    return newres.includes(searchText);
+                  })
+                  setListOfRestaurant(temp)
+                }
+              }}
+              ></input>
               <button id="search-btn" onClick={()=>{
                 // console.log(searchText);
                 const temp=resObj.filter((res)=>{
@@ -49,7 +75,7 @@ const Body=()=>{
               }}>Search</button>
             </div>
             <button className="filter-btn" onClick={()=>{
-                const tempObj=listOfRestaurant.filter(
+                const tempObj=resObj.filter(
                     (res)=>res.info.avgRating>4.5);
                 setListOfRestaurant(tempObj);
             }}>Top Rated Restaurant
